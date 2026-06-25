5822aa106ab2533b371764faaccd0c569cce8d91
fengjutian <fjutian@foxmail.com>
2026-06-25 13:03:43 +0800

    feat(core): 添加安全存储修复脚本
    添加了_fix_secure.py脚本来处理安全存储相关的修复工作，
    包括文件操作和正则表达式处理功能。

---
+++ _fix_secure.py
@@ -0,0 +1,3 @@
+f = open(r'D:\github\word_ninja\_fix_secure.py', 'w', encoding='utf-8')  
+f.write('''import re  
+with open(r\"D:\\github\\word_ninja\\word_ninja\\packages\\core\\lib\\storage\\secure_storage.dart\", \"r\", encoding=\"utf-8\") as fh: 
